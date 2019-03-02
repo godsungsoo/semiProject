07876bc2ddafdfcd874fc1778a6ee999c9c8652e
kdw912001 <45003222+kdw912001@users.noreply.github.com>
2019-03-02 21:03:50 +0900

    폭망

--- src/dogHouse/login/controller/loginValidServlet.java
+++ src/dogHouse/login/controller/loginValidServlet.java
@@ -1,7 +1,9 @@
 package dogHouse.login.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.sun.javafx.collections.MappingChange.Map;
 
 
 
@@ -36,8 +39,23 @@ public class loginValidServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		String param = request.getParameter("data");
 		
+		Gson gson = new Gson();
+		HashMap<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap = gson.fromJson(param, paramMap.getClass());
 		
-		System.out.println(param);
+		System.out.println(paramMap.get("userId"));
+		System.out.println(paramMap.get("userPwd"));
+		
+		//로그인 성공시 메인으로
+		if(paramMap.get("userId").equals("test")) {
+			System.out.println("아이디가 test");
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/main/main.jsp");
+			rd.forward(request, response);
+		}else {
+			System.out.println("else");
+			response.getWriter().append("fail");
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/login/login.jsp");
+		}
 	}
 
 }
